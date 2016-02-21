d402a7356c8ad7f6b5ed66ec16a95611f85310ad
Sanil <sanil.jain15@gmail.com>
2016-02-21 00:58:36 -0500

    Sidebar and Sidebar Controller Implemented

--- public/assignment/views/sidebar/sidebar.controller.js
+++ public/assignment/views/sidebar/sidebar.controller.js
@@ -1,3 +1,38 @@
 /**
  * Created by Sanil on 2/18/2016.
  */
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("SidebarController",SidebarController)
+
+    function SidebarController($scope){
+        //$rootScope=null;
+
+        $scope.checkLogin=checkLogin;
+        $scope.checkAdmin=checkAdmin;
+
+        function checkLogin(){
+
+            if($rootScope == null)
+                return false;
+
+            else
+                return true;
+        }
+
+        function checkAdmin(){
+
+            if($rootScope!=null)
+                for(var i=0;i<$rootScope.roles.length;i++)
+                {
+                    if($rootScope.roles[i]=="admin")
+                        return true;
+                }
+
+            return false;
+        }
+
+    }
+
+})();
