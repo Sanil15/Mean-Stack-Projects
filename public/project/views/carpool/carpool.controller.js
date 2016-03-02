c846ffb930ba10c4c2d81163c1c1661117b39db2
Sanil <sanil.jain15@gmail.com>
2016-03-02 16:28:22 -0500

    CRUD for entity carpool

--- public/project/views/carpool/carpool.controller.js
+++ public/project/views/carpool/carpool.controller.js
@@ -1,3 +1,29 @@
 /**
  * Created by Sanil on 3/1/2016.
  */
+(function(){
+
+    'use strict'
+
+    angular
+        .module("CarPoolApp")
+        .controller("CarPoolController",CarPoolController)
+
+        function CarPoolController($scope,UserService, CarPoolService, $location){
+
+            $scope.createCarPool=createCarPool;
+
+            function createCarPool(){
+                var userId=UserService.getCurrentUser();
+                CarPoolService.createCarPoolByUser(userId,$scope.pool,render);
+            }
+
+            // function for callBack for registered user
+            function render(pool) {
+                console.log(pool);
+                $location.path("/home");
+            }
+
+        }
+
+})();
