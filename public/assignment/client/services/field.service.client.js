e6bc3865492582858c678e0c7a2d720081edaed0
Sanil <sanil.jain15@gmail.com>
2016-03-18 14:10:22 -0400

    Implemented field.service.client.js

---
+++ public/assignment/client/services/field.service.client.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Sanil on 3/18/2016.
+ */
+(function(){
+
+    'use strict';
+
+    angular
+        .module("FormBuilderApp")
+        .factory("FieldService",FieldService)
+
+    function FieldService($http) {
+
+        var api = {
+            createFieldForForm: createFieldForForm,
+            getFieldsForForm: getFieldsForForm,
+            getFieldForForm: getFieldForForm,
+            deleteFieldFromForm: deleteFieldFromForm,
+            updateField: updateField
+        }
+
+        return api;
+
+        function createFieldForForm(formId, field){
+
+            return $http.post("api/assignment/form/"+formId+"/field",field);
+        }
+
+        function getFieldsForForm(formId){
+            return $http.get("/api/assignment/form/"+formId+"/field");
+        }
+
+        function getFieldForForm(formId, fieldId) {
+            return $http.get("/api/assignment/form/"+formId+"/field/"+fieldId);
+        }
+
+        function deleteFieldFromForm(formId, fieldId) {
+            return $http.delete("/api/assignment/form/"+formId+"/field/"+fieldId);
+        }
+
+        function updateField(formId, fieldId, field) {
+            return $http.put("/api/assignment/form/"+formId+"/field/"+fieldId,field);
+        }
+    }
+
+})();
