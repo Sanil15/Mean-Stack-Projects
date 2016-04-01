8b6bc3ae3a8f2ffcc5cee7d54a4d15c133a81237
Sanil <sanil.jain15@gmail.com>
2016-04-01 18:19:13 -0400

    Changes and Refactoring

--- server.js
+++ server.js
@@ -10,7 +10,22 @@ app.use(bodyParser.json());
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 var mongoose = require("mongoose");
-var db = mongoose.connect('mongodb://localhost/form-maker');
+
+
+var connectionString = 'mongodb://127.0.0.1:27017/CS5610';
+
+// use remote connection string
+// if running in remote server
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
+
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
