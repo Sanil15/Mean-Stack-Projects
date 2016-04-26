a45d9de365b9388cdbefe8f08c6fa607ad6d10f0
Sanil <sanil.jain15@gmail.com>
2016-04-26 04:26:55 -0400

    CHANGES IN LOGIN AND REGISTER

--- public/project/client/views/users/login.controller.js
+++ public/project/client/views/users/login.controller.js
@@ -13,18 +13,18 @@
         var vm=this;
 
         vm.login=login;
-
-        $(document).ready(function(){
-            $(document).mousemove(function(e){
-                TweenLite.to($('XPS'),
-                    .5,
-                    { css:
-                    {
-                        backgroundPosition: ""+ parseInt(event.pageX/8) + "px "+parseInt(event.pageY/'12')+"px, "+parseInt(event.pageX/'15')+"px "+parseInt(event.pageY/'15')+"px, "+parseInt(event.pageX/'30')+"px "+parseInt(event.pageY/'30')+"px"
-                    }
-                    });
-            });
-        });
+        //
+        //$(document).ready(function(){
+        //    $(document).mousemove(function(e){
+        //        TweenLite.to($('.login_body'),
+        //            .5,
+        //            { css:
+        //            {
+        //                backgroundPosition: ""+ parseInt(event.pageX/8) + "px "+parseInt(event.pageY/'12')+"px, "+parseInt(event.pageX/'15')+"px "+parseInt(event.pageY/'15')+"px, "+parseInt(event.pageX/'30')+"px "+parseInt(event.pageY/'30')+"px"
+        //            }
+        //            });
+        //    });
+        //});
 
 
         function init() {
--- public/project/client/views/users/register.controller.js
+++ public/project/client/views/users/register.controller.js
@@ -15,51 +15,14 @@
 
             vm.register = register;
 
+
             function init() {
-                // nice form step wizard
-                $(document).ready(function () {
 
-                    var navListItems = $('div.setup-panel div a'),
-                        allWells = $('.setup-content'),
-                        allNextBtn = $('.nextBtn');
+// nice form step wizard
+                vm.x =1;
 
-                    allWells.hide();
-
-                    navListItems.click(function (e) {
-                        e.preventDefault();
-                        var $target = $($(this).attr('href')),
-                            $item = $(this);
-
-                        if (!$item.hasClass('disabled')) {
-                            navListItems.removeClass('btn-primary').addClass('btn-default');
-                            $item.addClass('btn-primary');
-                            allWells.hide();
-                            $target.show();
-                            $target.find('input:eq(0)').focus();
-                        }
-                    });
-
-                    allNextBtn.click(function(){
-                        var curStep = $(this).closest(".setup-content"),
-                            curStepBtn = curStep.attr("id"),
-                            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-                            curInputs = curStep.find("input[type='text'],input[type='email'],select[id='industry']"),
-                            isValid = true;
-
-                        $(".form-group").removeClass("has-error");
-                        for(var i=0; i<curInputs.length; i++){
-                            if (!curInputs[i].validity.valid){
-                                isValid = false;
-                                $(curInputs[i]).closest(".form-group").addClass("has-error");
-                            }
-                        }
-
-                        if (isValid)
-                            nextStepWizard.removeAttr('disabled').trigger('click');
-                    });
-
-                    $('div.setup-panel div a.btn-primary').trigger('click');
-                });
+                vm.show2=false;
+                vm.show3=false;
 
                 $("input[type=password]").keyup(function(){
                     var ucase = new RegExp("[A-Z]+");
@@ -118,7 +81,61 @@
                 });
 
 
+                // nice form step wizard
+                $(document).ready(function () {
 
+                    var navListItems = $('div.setup-panel div a'),
+                        allWells = $('.setup-content'),
+                        allNextBtn = $('.nextBtn'),
+                        allPrevBtn = $('.prevBtn');
+
+                    allWells.hide();
+
+                    navListItems.click(function (e) {
+                        e.preventDefault();
+                        var $target = $($(this).attr('href')),
+                            $item = $(this);
+
+                        if (!$item.hasClass('disabled')) {
+                            navListItems.removeClass('btn-primary').addClass('btn-default');
+                            $item.addClass('btn-primary');
+                            allWells.hide();
+                            $target.show();
+                            $target.find('input:eq(0)').focus();
+                        }
+                    });
+
+                    allNextBtn.click(function(){
+                        var curStep = $(this).closest(".setup-content"),
+                            curStepBtn = curStep.attr("id"),
+                            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+                            curInputs = curStep.find("input[type='text'],input[type='url']"),
+                            isValid = true;
+
+                        $(".form-group").removeClass("has-error");
+                        for(var i=0; i<curInputs.length; i++){
+                            if (!curInputs[i].validity.valid){
+                                isValid = false;
+                                $(curInputs[i]).closest(".form-group").addClass("has-error");
+                            }
+                        }
+
+                        if (isValid) {
+                            nextStepWizard.removeAttr('disabled').trigger('click');
+                        }
+                    });
+
+                    allPrevBtn.click(function(){
+                        var curStep = $(this).closest(".setup-content"),
+                            curStepBtn = curStep.attr("id"),
+                            prevStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().prev().children("a");
+
+                        $(".form-group").removeClass("has-error");
+                        prevStepWizard.removeAttr('disabled').trigger('click');
+                    });
+
+                    $('div.setup-panel div a.btn-primary').trigger('click');
+                });
             }
 
             init();
@@ -128,7 +145,7 @@
                 console.log("HI");
                 console.log(user);
                 console.log(confirmPassword)
-                if(user.password==confirmPassword){
+                if(user.password==confirmPassword && user.password!=null && confirmPassword!=null){
                     console.log("HI");
                     UserService.createUser(user)
                         .then(function (response){
